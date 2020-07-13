76a229a29d6c37eb7631f368ba1495f5ca6b07c7
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-13 17:34:31 +0800

    8th commit - Configuring Latest Extent Reports Step by Step

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -1,13 +1,18 @@
 package testcases;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
@@ -56,6 +61,20 @@ public class TestCase1 {
 	public void isSkip() {
 		test = extent.createTest("Skip Test");
 		throw new SkipException("Skipping the test case");
+	}
+	
+	@AfterMethod
+	public void tearDown(ITestResult result) {
+		if(result.getStatus() == ITestResult.FAILURE) {
+			
+		} else if(result.getStatus() == ITestResult.SKIP) {
+			
+		} else if(result.getStatus() == ITestResult.SUCCESS) {
+			String methodName = result.getMethod().getMethodName();
+			String logText = "<b>" + "TEST CASE: - " + methodName.toUpperCase() + "  PASSED" + "</b>";
+			Markup m = MarkupHelper.createLabel(logText, ExtentColor.GREEN);
+			test.pass(m);
+		}
 	}
 
 }
