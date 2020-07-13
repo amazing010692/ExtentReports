e6457d38adc21a252635caddaa84c6027590f6df
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-13 17:03:07 +0800

    4th commit - Configuring Latest Extent Reports Step by Step

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -1,5 +1,7 @@
 package testcases;
 
+import org.testng.annotations.BeforeTest;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
@@ -9,5 +11,11 @@ public class TestCase1 {
 	public ExtentSparkReporter htmlReporter;
 	public ExtentReports extent;
 	public ExtentTest test;
+	
+	@BeforeTest
+	public void setReport() {
+		htmlReporter = new ExtentSparkReporter("./reports/extent.html");
+		htmlReporter.config().setEncoding("utf-8");
+	}
 
 }
