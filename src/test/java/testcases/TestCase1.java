8967f17bb6ba5618b2804410a9870ee953e3633f
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-14 13:33:18 +0800

    13th commit - Configuring Latest Extent Reports Step by Step

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -1,5 +1,7 @@
 package testcases;
 
+import java.util.Arrays;
+
 import org.testng.ITestResult;
 import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
@@ -9,6 +11,7 @@ import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
@@ -16,11 +19,11 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class TestCase1 {
-	
+
 	public ExtentHtmlReporter htmlReporter;
 	public ExtentReports extent;
 	public ExtentTest test;
-	
+
 	@BeforeTest
 	public void setReport() {
 		htmlReporter = new ExtentHtmlReporter("./reports/extent.html");
@@ -28,51 +31,70 @@ public class TestCase1 {
 		htmlReporter.config().setDocumentTitle("JJSG'S Test Automation Reports");
 		htmlReporter.config().setReportName("Automation Test Results");
 		htmlReporter.config().setTheme(Theme.DARK);
-		
+
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
-		
+
 		extent.setSystemInfo("Test Automation Engineer", "Janielle Joy Gregorio");
 		extent.setSystemInfo("Organization", "Jan's Group of Testers");
 		extent.setSystemInfo("Build No.", "JJSG-amazing010692");
 	}
-	
+
 	@AfterTest
 	public void endReport() {
 		extent.flush();
 	}
-	
-	//Pass, Fail and Skip a Test.
-	
+
+	// Pass, Fail and Skip a Test.
+
 	@Test
 	public void doLogin() {
 		test = extent.createTest("Login Test");
 		System.out.println("Executing Login Test");
 	}
-	
+
 	@Test
 	public void doUserReg() {
 		test = extent.createTest("User Registration Test");
-		//Assert.fail("User Registration Test Failed");
+		// Assert.fail("User Registration Test Failed");
 	}
-	
+
 	@Test
 	public void isSkip() {
 		test = extent.createTest("Skip Test");
 		throw new SkipException("Skipping the test case");
 	}
-	
+
 	@AfterMethod
 	public void tearDown(ITestResult result) {
-		if(result.getStatus() == ITestResult.FAILURE) {
-			
-		} else if(result.getStatus() == ITestResult.SKIP) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			String exceptionMessage = Arrays.toString(result.getThrowable().getStackTrace());
+			test.fail("<details>" + "<summary>" + "<b>" + "<font color=" + "red>" + "Exception Occured:Click to see"
+					+ "</font>" + "</b >" + "</summary>" + exceptionMessage.replaceAll(",", "<br>") + "</details>"
+					+ " \n");
+
+			/*//Capture screenshot in case of a failure.
+			 * try {
+			 * 
+			 * ExtentManager.captureScreenshot(); testReport.get().fail("<b>" +
+			 * "<font color=" + "red>" + "Screenshot of failure" + "</font>" + "</b>",
+			 * MediaEntityBuilder.createScreenCaptureFromPath(ExtentManager.screenshotName)
+			 * .build()); } catch (IOException e) {
+			 * 
+			 * }
+			 */
+
+			String failureLogg = "TEST CASE FAILED";
+			Markup m = MarkupHelper.createLabel(failureLogg, ExtentColor.RED);
+			test.log(Status.FAIL, m);
+
+		} else if (result.getStatus() == ITestResult.SKIP) {
 			String methodName = result.getMethod().getMethodName();
 			String logText = "<b>" + "TEST CASE: - " + methodName.toUpperCase() + "  SKIPPED" + "</b>";
 			Markup m = MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
 			test.skip(m);
-			
-		} else if(result.getStatus() == ITestResult.SUCCESS) {
+
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
 			String methodName = result.getMethod().getMethodName();
 			String logText = "<b>" + "TEST CASE: - " + methodName.toUpperCase() + "  PASSED" + "</b>";
 			Markup m = MarkupHelper.createLabel(logText, ExtentColor.GREEN);
