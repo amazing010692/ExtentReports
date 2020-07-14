9371100754e3ba50986f8fffb93905ac1d43ade4
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-15 00:58:49 +0800

    7th commit - Generating extent reports through Customized Listeners

--- src/test/java/testcases/TestCase2.java
+++ src/test/java/testcases/TestCase2.java
@@ -1,5 +1,24 @@
 package testcases;
 
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
 public class TestCase2 {
+	
+	@Test
+	public void doLogin() {
+		System.out.println("Executing Login Test");
+	}
+
+	@Test
+	public void doUserReg() {
+		Assert.fail("User Registration Test Failed");
+	}
+
+	@Test
+	public void isSkip() {
+		throw new SkipException("Skipping the test case");
+	}
 
 }
